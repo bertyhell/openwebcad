86576665e2cb10b5fd1957219a87f0af06a5092b
Bert Verhelst <verhelstbert@gmail.com>
2025-05-03 14:16:06 +0200

    fix: switch vite base path to ./

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
+import {defineConfig} from 'vite';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
-	base: '/openwebcad/',
+	base: './',
 	build: {
 		outDir: 'dist',
 	},

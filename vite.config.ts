bc6dff7e1f29a34d816e992c28b1c61288813277
Bert Verhelst <verhelstbert@gmail.com>
2025-05-03 14:22:51 +0200

    fix: update vite config base path

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import svgr from 'vite-plugin-svgr'; // https://vitejs.dev/config/
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react(), svgr()],
+	base: './',
 	build: {
 		outDir: 'dist',
 	},

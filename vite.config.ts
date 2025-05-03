33977bc26647373623907437e90802ad8565d6da
Bert Verhelst <verhelstbert@gmail.com>
2025-05-03 13:48:11 +0200

    feat: switch to building with github actions and gh-pages branch

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr()],
-  build: {
-    outDir: 'docs',
-  },
+	plugins: [react(), svgr()],
+	base: '/openwebcad/',
+	build: {
+		outDir: 'dist',
+	},
 });
